150a331ebfd3e37665a26a6ef915a0ea016cfab8
Denis <grabli1989@gmail.com>
2017-04-25 16:19:28 +0300

    booking

---
+++ script.js
@@ -0,0 +1,51 @@
+$(function () {
+    var b_checkin_day = $('#b_checkin_day'),
+        b_checkin_month = $('#b_checkin_month'),
+        b_checkout_day = $('#b_checkout_day'),
+        b_checkout_month = $('#b_checkout_month');
+
+    var dateFormat = "dd MM `y",
+        from = $("#b_checkin_from")
+            .datepicker({
+                dateFormat: "dd MM `y",
+                setDate: +7,
+                changeMonth: true,
+                numberOfMonths: 3
+            })
+            .datepicker("setDate", new Date())
+            .on("change", function () {
+                to.datepicker("option", "minDate", getDate(this, 'from'));
+            }),
+        to = $("#b_checkin_to")
+            .datepicker({
+                dateFormat: "dd MM `y",
+                setDate: +7,
+                changeMonth: true,
+                numberOfMonths: 3
+            })
+            .datepicker("setDate", +1)
+            .on("change", function () {
+                from.datepicker("option", "maxDate", getDate(this, 'to'));
+            });
+
+    function getDate(element, fromto) {
+        var date;
+        try {
+            date = $.datepicker.parseDate(dateFormat, element.value);
+        } catch (error) {
+            date = null;
+        }
+
+        switch (fromto) {
+            case 'from':
+                b_checkin_day.val(date.getDate().toString());
+                b_checkin_month.val(date.getFullYear() + '-' +(date.getMonth()+1));
+                break;
+            case 'to':
+                b_checkout_day.val(date.getDate().toString());
+                b_checkout_month.val(date.getFullYear() + '-' +(date.getMonth()+1));
+                break;
+        }
+        return date;
+    }
+});
