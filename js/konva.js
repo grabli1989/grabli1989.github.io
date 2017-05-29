6a234453de5835f1bfe6fd6e51fabd20a5ef3dda
Denis <grabli1989@gmail.com>
2017-05-29 19:29:41 +0300

    remarks fix

--- js/konva.js
+++ js/konva.js
@@ -7,6 +7,8 @@ var anchor1Group = null;
 var ratioImage1 = null;
 var newImage = null;
 var imageGroup = null;
+var groups = {}
+var animatedLayer = null;
 
 var resizeContainer = function () {
     if(stage) {
@@ -70,6 +72,7 @@ var update = function (activeAnchor) {
             // bottomRight.setY(width * ratioImage1);
         }
 
+        //
         if(image.getParent().getAttr('name') == 'imageGroup') {
             imageGroup.offsetX(image.getWidth()/2);
             imageGroup.offsetY(image.getHeight()/2);
@@ -215,198 +218,199 @@ var imageContainerRender = function () {
 
 
 var addNewImage = function (slideNum) {
-    // reset();
-    var width = 128,
-        height = 128;
-    if(!stage || !layer || !imageContainer) {
-        return;
-    }
-
-    var x = imageContainer.width()/2;
-    var y = imageContainer.height()/2;
-
-    var animatedLayer = new Konva.Layer();
-
-    var center = new Konva.Circle({
-        x: width / 2,
-        y: -20,
-        radius: 15,
-        fill: '#555',
-        name: 'center',
-        opacity: 0,
-        // draggable: true,
-        dragOnTop: false
-    });
-    center.on('mousedown touchstart', function () {
-        imageGroup.draggable(false);
-    });
-
-    center.on('mouseup touchend dragend', function () {
-        imageGroup.draggable(true);
-    });
 
     if(imageGroup) {
-        var image = imageGroup.getChildren(function (node) {
-            return node.getAttr('name') === 'image';
+        var id = imageGroup.id();
+        groups[id] = imageGroup.clone({
+            id: id,
         });
-        image.destroy();
+        newImage.destroy();
         imageGroup.destroy();
     }
-    var image = new Konva.Image({
-        width: width,
-        height: height,
-        name: 'image',
-        // draggable: true
-    });
 
-    newImage = image;
-
-    image.on('mousedown touchstart', function () {
-        imageGroup.draggable(true);
-        imageGroup.controlled = false;
-    });
-
-    image.on('mouseover tap', function () {
-        center.opacity(1);
-        anchorGroup.opacity(1);
-        $('#brightness').show();
-    });
-    image.on('mouseout', function () {
-        // anchorGroup.opacity(0);
-    });
-
-
-    stage.on('click tap', function (evt) {
-        var targetName = evt.target.getAttr('name');
-        if(targetName != 'image') {
-            $('#brightness').hide();
-        }
-        switch (targetName) {
-            case 'image1':
-                anchorGroup.opacity(0);
-                center.opacity(0);
-                anchor1Group.opacity(1);
-                break;
-            case 'image':
-                anchorGroup.opacity(1);
-                center.opacity(1);
-                anchor1Group.opacity(0);
-                break;
-        }
-
-    });
-
-    imageGroup = new Konva.Group({
-        width: width,
-        height: height,
-        offsetX: width / 2,
-        offsetY: height / 2,
-        x: x,
-        y: y,
-        brightness: 1,
-        name: 'imageGroup',
-        draggable: true
-    });
-
-    animatedLayer.add(imageGroup);
-    // imageGroup.add(image);
-    imageGroup.add(center);
-
-    // imageGroup.lastRotation = 0;
-    // imageGroup.angularVelocity = 6;
-    imageGroup.controlled = false;
-    center.on('mousedown touchstart touchmove', function(evt) {
-        imageGroup.angularVelocity = 0;
-        imageGroup.controlled = true;
-    });
-
-    stage.on('contentMouseup touchend', function() {
-        imageGroup.controlled = false;
-    });
-    stage.on('contentMousemove touchstart touchmove', function() {
-        if(imageGroup.controlled) {
-            var mousePos = stage.getPointerPosition();
-            var x = imageGroup.getX() - mousePos.x;
-            var y = imageGroup.getY() - mousePos.y;
-            imageGroup.setRotation(0.5 * Math.PI + Math.atan(y / x));
-            if(mousePos.x <= imageGroup.getX()) {
-                imageGroup.rotate(Math.PI);
-            }
-            // imageGroup.offsetX(image.getWidth()/2);
-            // imageGroup.offsetY(image.getHeight()/2);
-        }
-    });
-    stage.add(animatedLayer);
-    var anim = new Konva.Animation(function(frame) {
-        animate(animatedLayer, imageGroup, frame);
-    }, animatedLayer);
-// wait one second and then spin the star
-    setTimeout(function() {
-        anim.start();
-    });
-
-    var anchorGroup = new Konva.Group({
-        width: width,
-        height: height,
-        // offsetX: width / 2,
-        // offsetY: height / 2,
-        name: 'anchorGroup',
-        draggable: true,
-        opacity: 0
-    });
-    imageGroup.add(anchorGroup);
-
-    addAnchor(anchorGroup, 0, 0, 'topLeft', 0);
-    addAnchor(anchorGroup, width, 0, 'topRight', 0);
-    var anchor = addAnchor(anchorGroup, width, height, 'bottomRight');
-    addAnchor(anchorGroup, 0, height, 'bottomLeft', 0);
-
-    var imageObj = new Image();
-    imageObj.onload = function() {
-        newImage.image(imageObj);
-
+    if(groups.hasOwnProperty(slideNum)) {
+        imageGroup = groups[slideNum];
+        animatedLayer.add(imageGroup);
+        image = imageGroup.getChildren(function (node) {
+            return node.getAttr('name') === 'image';
+        })[0];
+        newImage = image;
         newImage.cache();
-        newImage.filters([Konva.Filters.Brighten]);
-        imageGroup.add(image);
-        var slider = document.getElementById('brightness');
-        slider.onchange = function() {
-            newImage.brightness(slider.value);
-            animatedLayer.batchDraw();
-
-            // var clone = newImage.clone({
-            //         brightness: image.brightness(),
-            //         width: image.width(),
-            //         height: image.height()
-            //     });
-            //     // clone.cache();
-            //     newImage.destroy();
-            //     newImage = clone;
-            //     imageGroup.add(image);
-            // animatedLayer.batchDraw();
-
-        };
-
-        // animatedLayer.draw();
-    };
-
-    anchor.on('dragmove', function() {
-        // console.log(newImage);
-        var clone = newImage.clone({
-            brightness: newImage.brightness(),
-            width: imageGroup.offsetX()*2,
-            height: imageGroup.offsetY()*2
-        });
-        clone.cache();
-        newImage.destroy();
-        newImage = clone;
-        imageGroup.add(newImage);
         animatedLayer.batchDraw();
 
-        center.setX(clone.width()/2);
-    });
+    } else {
+        var width = 128,
+            height = 128;
+        if(!stage || !layer || !imageContainer) {
+            return;
+        }
 
-    imageObj.src = '/img/clickable/img' + slideNum + '.png';
-    imageObj.id = 'slide'+slideNum;
+        var x = imageContainer.width()/2;
+        var y = imageContainer.height()/2;
+
+        animatedLayer = new Konva.Layer();
+
+        var center = new Konva.Circle({
+            x: width / 2,
+            y: -20,
+            radius: 15,
+            fill: '#555',
+            name: 'center',
+            opacity: 0,
+            // draggable: true,
+            dragOnTop: false
+        });
+        center.on('mousedown touchstart', function () {
+            imageGroup.draggable(false);
+        });
+
+        center.on('mouseup touchend dragend', function () {
+            imageGroup.draggable(true);
+        });
+
+        var image = new Konva.Image({
+            width: width,
+            height: height,
+            name: 'image',
+            // draggable: true
+        });
+
+        newImage = image;
+
+        image.on('mousedown touchstart', function () {
+            imageGroup.draggable(true);
+            imageGroup.controlled = false;
+        });
+
+        image.on('mouseover tap', function () {
+            center.opacity(1);
+            anchorGroup.opacity(1);
+            $('#brightness').show();
+        });
+        image.on('mouseout', function () {
+            // anchorGroup.opacity(0);
+        });
+
+
+        stage.on('click tap', function (evt) {
+            var targetName = evt.target.getAttr('name');
+            if(targetName != 'image') {
+                $('#brightness').hide();
+            }
+            switch (targetName) {
+                case 'image1':
+                    anchorGroup.opacity(0);
+                    center.opacity(0);
+                    anchor1Group.opacity(1);
+                    break;
+                case 'image':
+                    anchorGroup.opacity(1);
+                    center.opacity(1);
+                    anchor1Group.opacity(0);
+                    break;
+            }
+
+        });
+
+        imageGroup = new Konva.Group({
+            width: width,
+            height: height,
+            offsetX: width / 2,
+            offsetY: height / 2,
+            x: x,
+            y: y,
+            name: 'imageGroup',
+            id: slideNum,
+            draggable: true
+        });
+
+        animatedLayer.add(imageGroup);
+        // imageGroup.add(image);
+        imageGroup.add(center);
+
+        // imageGroup.lastRotation = 0;
+        // imageGroup.angularVelocity = 6;
+        imageGroup.controlled = false;
+        center.on('mousedown touchstart touchmove', function(evt) {
+            imageGroup.angularVelocity = 0;
+            imageGroup.controlled = true;
+        });
+
+        stage.on('contentMouseup touchend', function() {
+            imageGroup.controlled = false;
+        });
+        stage.on('contentMousemove touchstart touchmove', function() {
+            if(imageGroup.controlled) {
+                var mousePos = stage.getPointerPosition();
+                var x = imageGroup.getX() - mousePos.x;
+                var y = imageGroup.getY() - mousePos.y;
+                imageGroup.setRotation(0.5 * Math.PI + Math.atan(y / x));
+                if(mousePos.x <= imageGroup.getX()) {
+                    imageGroup.rotate(Math.PI);
+                }
+                // imageGroup.offsetX(image.getWidth()/2);
+                // imageGroup.offsetY(image.getHeight()/2);
+            }
+        });
+        stage.add(animatedLayer);
+        var anim = new Konva.Animation(function(frame) {
+            animate(animatedLayer, imageGroup, frame);
+        }, animatedLayer);
+// wait one second and then spin the star
+        setTimeout(function() {
+            anim.start();
+        });
+
+        var anchorGroup = new Konva.Group({
+            width: width,
+            height: height,
+            // offsetX: width / 2,
+            // offsetY: height / 2,
+            name: 'anchorGroup',
+            draggable: true,
+            opacity: 0
+        });
+        imageGroup.add(anchorGroup);
+
+        addAnchor(anchorGroup, 0, 0, 'topLeft', 0);
+        addAnchor(anchorGroup, width, 0, 'topRight', 0);
+        var anchor = addAnchor(anchorGroup, width, height, 'bottomRight');
+        addAnchor(anchorGroup, 0, height, 'bottomLeft', 0);
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+            newImage.image(imageObj);
+
+            newImage.cache();
+            newImage.filters([Konva.Filters.Brighten]);
+            imageGroup.add(image);
+            var slider = document.getElementById('brightness');
+            slider.onchange = function() {
+                newImage.brightness(slider.value);
+                animatedLayer.batchDraw();
+            };
+        };
+
+        anchor.on('dragmove', function() {
+            // console.log(newImage);
+            var clone = newImage.clone({
+                brightness: newImage.brightness(),
+                width: imageGroup.offsetX()*2,
+                height: imageGroup.offsetY()*2
+            });
+            clone.cache();
+            newImage.destroy();
+            newImage = clone;
+            imageGroup.add(newImage);
+            animatedLayer.batchDraw();
+
+            center.setX(clone.width()/2);
+        });
+
+        imageObj.src = '/img/clickable/img' + slideNum + '.png';
+        imageObj.id = 'slide'+slideNum;
+    }
+    console.log(groups);
 }
 
 // Rotation
